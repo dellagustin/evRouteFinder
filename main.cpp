e2045431ed30b601b2f0ca70b388aad1f20629b2
Guilherme Dellagustin <dellagustin@gmail.com>
2011-03-31 07:46:57 -0300

    Firts commmit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "../reuse/ge_points.h"
+
+using namespace std;
+
+class CWayPoint : public CGePoint2d
+{
+private:
+	bool m_bIsFixed;
+};
+
+class CRoute // : vector<CWayPoint>
+{
+	// members
+private:
+	int m_nWayPoints;
+};
+
+int main(int argc, const char *argv[])
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
